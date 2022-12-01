926b00c72b091df231ac6c737628021b9266acbb
Nerixyz <nerixdev@outlook.de>
2022-12-01 17:16:34 +0100

    fix: don't clear stuff
    I really only care about the fast forward. I guess I'll have to use one of these user-script adblockers that feature the worst codestlye. Honestly, I'd rather watch ads than run that code.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,11 +1,7 @@
 import _OnBeforeRequestDetails = browser.webRequest._OnBeforeRequestDetails;
 import { StreamFilter } from '../types';
-import { parseAttributes } from './utilities/m3u8.utilities';
 import { OverridePlayer, UserAgent } from '../options';
-import { onAdPod, StreamTabs } from './ad.replacement';
-import { TWITCH_USER_PAGE } from './utilities/request.utilities';
-import { eventHandler } from './utilities/messaging';
-import { TwitchStitchedAdData } from './twitch-m3u8.types';
+import { StreamTabs } from './ad.replacement';
 function onRequest(request: _OnBeforeRequestDetails) {
   if (!request.url.includes('video-weaver')) return;
 
@@ -28,8 +24,8 @@ function onRequest(request: _OnBeforeRequestDetails) {
       filter.close();
     };
 
-    extractAdData(text, request.documentUrl ?? '', request.tabId);
-    finalWrite(cleanupAllAdStuff(text));
+    //extractAdData(text, request.documentUrl ?? '', request.tabId);
+    finalWrite(text);
   };
 
   filter.onstop = () => {
@@ -81,29 +77,3 @@ browser.webRequest.onBeforeSendHeaders.addListener(
   },
   ['requestHeaders', 'blocking']
 );
-
-function cleanupAllAdStuff(data: string) {
-  return data
-    .replace(/X-TV-TWITCH-AD-URL="[^"]+"/g, 'X-TV-TWITCH-AD-URL="javascript:alert(\'pogo\')"')
-    .replace(
-      /X-TV-TWITCH-AD-CLICK-TRACKING-URL="[^"]+"/g,
-      'X-TV-TWITCH-AD-CLICK-TRACKING-URL="javascript:alert(\'pogo\')"'
-    )
-    .replace(/X-TV-TWITCH-AD-ADVERIFICATIONS="[^"]+"/g, `X-TV-TWITCH-AD-ADVERIFICATIONS="${btoa('{}')}"`)
-    .replace(/#EXT-X-DATERANGE.+CLASS=".*ad.*".+\n/g, '');
-}
-
-function extractAdData(data: string, doc: string, tabId: number) {
-  const attrString = data.match(/#EXT-X-DATERANGE:(ID="stitched-ad-[^\n]+)\n/)?.[1];
-  if (!attrString) {
-    console.warn('no stitched ad');
-    return;
-  }
-  if(!TWITCH_USER_PAGE.test(doc)) return;
-
-  const attr = parseAttributes<TwitchStitchedAdData>(attrString);
-  onAdPod(attr, StreamTabs.get(tabId) ?? TWITCH_USER_PAGE.exec(doc)?.[1] ?? '').then(() => {
-    eventHandler.emitContext('adSkipped', {});
-    console.debug('"Skipped" ad.');
-  }).catch(console.error);
-}
